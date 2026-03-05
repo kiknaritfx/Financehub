b3a473b8cc70aba2ebb0f82ca6ca7459d3fdafad
Kik Narit <kiknarit@MacBook-Pro-khxng-Kik.local>
2026-03-05 12:24:24 +0700

    Fix [...path].js handler

--- api/[...path].js
+++ api/[...path].js
@@ -1,16 +1,10 @@
+
 import app from '../server/index.js';
 
 export default async function handler(req, res) {
-  const originalUrl = req.url;
-  
-  // Vercel sends path WITHOUT /api prefix to this catch-all handler
-  // e.g. PUT /api/businesses/3 → req.url = '/businesses/3'
-  // The route() helper in server registers BOTH /api/xxx and /xxx
-  // So we just need to make sure it doesn't have double /api
-  if (req.url.startsWith('/api/api/')) {
-    req.url = req.url.replace('/api/api/', '/api/');
+  if (!req.url.startsWith('/api/')) {
+    req.url = '/api' + req.url;
   }
-
   return new Promise((resolve, reject) => {
     app(req, res);
     res.on('finish', resolve);
