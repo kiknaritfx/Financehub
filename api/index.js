2c682b29e7be068816506d1dda0769b776740bd1
Kik Narit <kiknarit@MacBook-Pro-khxng-Kik.local>
2026-03-06 11:48:44 +0700

    Fix vercel.json and add index.js

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import app from "../server/index.js";
+
+export default async function handler(req, res) {
+  if (!req.url.startsWith("/api/")) {
+    req.url = "/api" + req.url;
+  }
+  return new Promise((resolve, reject) => {
+    app(req, res);
+    res.on("finish", resolve);
+    res.on("error", reject);
+  });
+}
